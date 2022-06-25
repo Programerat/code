703d51e8a72de09921aba654ed90a8a27b94d037
Diar <diarselimi92@gmail.com>
2022-06-26 00:31:15 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState, useCallback, useRef, createRef} from 'react';
+import Button from '@mui/material/Button';
+import {AppBar, Box, ButtonGroup, FormControl, Grid, IconButton, InputLabel, MenuItem, Select, TextareaAutosize, TextField, Toolbar, Typography } from '@mui/material';
+import Highlight from 'react-highlight'
+import "../node_modules/highlight.js/styles/atom-one-dark.css";
+import { toPng, toSvg } from 'html-to-image';
 
-function App() {
+const paddingSize = [
+  <Button key="one">20</Button>,
+  <Button key="two">40</Button>,
+  <Button key="three">60</Button>,
+];
+
+const App = () => {
+  const [code, setCode] = useState('');
+  const [beforeCodeText, setBeforeCodeText] = useState('');
+  const [afterCodeText, setAfterCodeText] = useState('');
+
+  const [background, setBackground] = useState('first');
+
+  const ref = createRef();
+
+  const handleChange = (event) => {
+    setBackground(event.target.value);
+  };
+
+  const updateResult = (content) => {
+    let beforeText = '';
+    let allCode = '';
+    let afterText = '';
+    let texts = content.split(/\n/gm);
+
+    console.log(texts);
+    let bfText = true;
+
+    texts.forEach(text => {
+      let textSplit = text.split(':');
+
+      if(textSplit.length === 1){
+        allCode += text.trimEnd()+'\n';
+      }
+
+      if (textSplit.length === 2) {
+        let tag = textSplit[0].trim();
+        if (tag === 'code') {
+          bfText = false;
+          allCode += textSplit[1].trimEnd()+'\n';
+        }
+
+        if(bfText){
+          beforeText += '<'+tag+'>'+textSplit[1].trim()+'</'+tag+'>';
+        } else {
+          afterText += '<'+tag+'>'+textSplit[1].trim()+'</'+tag+'>';
+        }
+      }
+    });
+
+    setCode(allCode);
+    setBeforeCodeText(beforeText);
+    setAfterCodeText(afterText);
+  }
+
+  const handleKeyPress = (event) => {
+    if(event.keyCode === 9){
+      //prevent and add 4 spaces
+      event.preventDefault();
+      let text = event.target.value; 
+      let newText = text.substring(0, event.target.selectionStart) + '    ' + text.substring(event.target.selectionStart);
+      event.target.value = newText;
+    }
+  }
+
+  const saveSvgImage = useCallback(() => {
+    if (ref.current === null) {
+      return;
+    }
+
+    toSvg(ref.current, {})
+    .then(function (dataUrl) {
+      var link = document.createElement('a');
+      link.download = 'image.svg';
+      link.href = dataUrl;
+      link.click();
+    }, [ref]);
+  });
+
+  const savePngImage = useCallback(() => {
+    if (ref.current === null) {
+      return
+    }
+
+    toPng(ref.current, {})
+      .then((dataUrl) => {
+
+        const link = document.createElement('a')
+        link.download = 'my-image-name.png'
+        link.href = dataUrl
+        link.click()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [ref]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid container 
+      spacing={2}
+      direction="row"
+      justifyContent="center"
+      alignItems="center" 
+    >
+      <AppBar position="static" sx={{ bgcolor: "#fff" }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <img src="https://avatars.githubusercontent.com/u/37701908?s=400&u=769b3ec1ff8bcf66dbfaf207e6876dfd85ef4aa0&v=4" width='30px' alt="logo" />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Programerat
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+            <Select
+              labelId="backgroundColor"
+              id="selectBgColor"
+              value={background}
+              label="Background"
+              onChange={handleChange}
+            >
+              <MenuItem className='first' value={'first'}>Primary</MenuItem>
+              <MenuItem className='second' value={'second'}>Secondary</MenuItem>
+              <MenuItem className='third' value={'third'}>Third</MenuItem>
+              <MenuItem className='fourth' value={'fourth'}>Fourth</MenuItem>
+              <MenuItem className='fifth' value={'fifth'}>Fifth</MenuItem>
+              <MenuItem className='sixth' value={'sixth'}>Sixth</MenuItem>
+              <MenuItem className='seven' value={'seven'}>Sixth</MenuItem>
+            </Select>
+            </FormControl>
+          </Box>
+          <ButtonGroup variant="outlined" aria-label="outlined button group">
+              <Button onClick={savePngImage}>Png</Button>
+              <Button onClick={saveSvgImage}>Svg</Button>
+            </ButtonGroup>
+        </Toolbar>
+      </AppBar>
+      <Grid item xs={2} md={3}>
+      </Grid>
+      <Grid 
+        item 
+        xs={8} 
+        md={6}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div ref={ref} className={background + ' small'} height="100%">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: beforeCodeText
+            }}></div>
+          { code && <Highlight className="php code">{code}</Highlight> }
+          <div
+            dangerouslySetInnerHTML={{
+              __html: afterCodeText
+            }}></div>
+          </div>
+          <Box mt={2}>
+            <TextField
+              multiline
+              label="Your content here..."
+              onKeyDown={handleKeyPress}
+              onChange={(e) => {updateResult(e.target.value)}}
+              aria-label="Code editor"
+              minRows={7}
+              placeholder="<?php echo 'code here';"
+              style={{ width: 500 }}
+            />
+          </Box>
+      </Grid>
+      <Grid item xs={2} md={3}>
+      </Grid>
+    </Grid>
   );
 }
 
