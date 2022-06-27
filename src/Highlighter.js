e3224cbf8cc8c470cdd05959d78beeced79e51b3
Diar <diarselimi92@gmail.com>
2022-06-27 12:03:12 +0200

    Fix code highlighter

--- src/Highlighter.js
+++ src/Highlighter.js
@@ -1,21 +1,11 @@
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import php from 'react-syntax-highlighter/dist/esm/languages/hljs/php';
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
-import bash from 'react-syntax-highlighter/dist/esm/languages/hljs/bash';
-import md from 'react-syntax-highlighter/dist/esm/languages/hljs/markdown';
+import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import docco from 'react-syntax-highlighter/dist/esm/styles/prism/atom-dark';
 
-SyntaxHighlighter.registerLanguage('php', php);
-SyntaxHighlighter.registerLanguage('javascript', js);
-SyntaxHighlighter.registerLanguage('css', css);
-SyntaxHighlighter.registerLanguage('bash', bash);
-SyntaxHighlighter.registerLanguage('markdown', md);
 
 const Code = ({language, code}) => {
   return (
-    <div className='code'>
-        <SyntaxHighlighter language={language} className='tbg' style={docco}>
+    <div className='code line-numbers'>
+        <SyntaxHighlighter language={language} className='tbg' style={docco} highlighter={"prism" || "hljs"}>
         {code}
         </SyntaxHighlighter>
     </div>
