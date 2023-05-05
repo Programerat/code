f4771103bb31961490c9f00fcb965e4f125060e7
diar <diar.selimi@nordsec.com>
2023-05-05 14:14:48 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -30,12 +30,47 @@ function generateRandomName(length) {
   return result;
 }
 
+const gradients = [
+  {
+    name: 'Blue-Purple-Pink',
+    class: 'bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500',
+    textColor: 'text-white',
+  },
+  {
+    name: 'Purple-Pink-Orange',
+    class: 'bg-gradient-to-br from-purple-400 via-pink-500 to-orange-500',
+    textColor: 'text-white',
+  },
+  {
+    name: 'Teal-Purple-Orange',
+    class: 'bg-gradient-to-br from-teal-400 via-purple-500 to-orange-500',
+    textColor: 'text-white',
+  },
+  {
+    name: 'Green-Blue-Purple',
+    class: 'bg-gradient-to-br from-green-400 via-blue-500 to-purple-500',
+    textColor: 'text-white',
+  },
+  {
+    name: 'Pink-Orange-Yellow',
+    class: 'bg-gradient-to-br from-pink-400 via-orange-500 to-yellow-500',
+    textColor: 'text-white',
+  },
+  {
+    name: 'Yellow-Green-Blue',
+    class: 'bg-gradient-to-br from-yellow-400 via-green-500 to-blue-500',
+    textColor: 'text-black',
+  },
+];
+
+
 
 
 function App() {
   let [content, setContent] = useState('');
   const [alignment, setAlignment] = useState('text-left');
   const [previewWidth, setPreviewWidth] = useState('w-full');
+  const [gradient, setGradient] = useState("bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500");
 
   const setCompactWidth = () => {
     setPreviewWidth('w-auto inline-block');
@@ -80,6 +115,24 @@ function App() {
         <div className="flex items-center justify-between mb-2">
           <h2 className="text-xl font-bold">Write Your Markdown</h2>
           <div className="text-sm">
+          <select class="mx-1 px-2 py-1 rounded-md bg-gray-200 hover:bg-gray-300" onChange={(e) => setGradient(e.target.value)}>
+            <option value="bg-gradient-to-r from-green-400 via-blue-500 to-blue-500">Green to Blue</option>
+            <option value="bg-gradient-to-tl from-red-400 via-yellow-500 to-yellow-500">Red to Yellow</option>
+            <option value="bg-gradient-to-tr from-indigo-400 via-teal-500 to-teal-500">Indigo to Teal</option>
+            <option value="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">Pink to Yellow</option>
+            <option value="bg-gradient-to-tr from-purple-400 via-pink-500 to-red-500">Purple to Red</option>
+            <option value="bg-gradient-to-r from-orange-400 via-yellow-500 to-green-500">Orange to Green</option>
+            <option value="bg-gradient-to-t from-purple-400 via-pink-500 to-red-500">Purple to Red (vertical)</option>
+            <option value="bg-gradient-to-br from-red-400 via-orange-500 to-yellow-500">Red to Yellow (diagonal)</option>
+            <option value="bg-gradient-to-br from-yellow-400 via-red-500 to-pink-500">Yellow to Pink</option>
+            <option value="bg-gradient-to-br from-purple-400 via-pink-500 to-red-500">Purple to Red</option>
+            <option value="bg-gradient-to-br from-green-500 via-blue-500 to-purple-500">Green to Purple</option>
+            <option value="bg-gradient-to-br from-red-500 via-orange-500 to-yellow-500">Red to Yellow</option>
+            <option value="bg-gradient-to-br from-gray-400 via-gray-500 to-gray-600">Gray</option>
+
+          </select>
+
+
             <button className="px-2 py-1 rounded-md bg-gray-200 hover:bg-gray-300 mr-1" onClick={() => setAlignment('text-left')}>
               <ArrowNarrowLeftIcon className="w-4 h-4" />
             </button>
@@ -113,9 +166,9 @@ function App() {
           </button>
         </div>
         <div id="markdownOutputContainer" className='flex items-center p-5'>
-        <div className={`text-white bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 p-4 shadow-lg rounded-lg ${previewWidth}`}>
+        <div className={`p-4 rounded-lg ${gradient} ${previewWidth}`} >
           <div
-            className={`${alignment} text-white prose dark:prose`} dangerouslySetInnerHTML={{__html: renderContent(content)}}
+            className={`${alignment} prose dark:prose`} dangerouslySetInnerHTML={{__html: renderContent(content)}}
           ></div>
         </div>
         </div>
